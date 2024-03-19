62ae3ca94c7d81750ec204e079a3407b91388b0f
IsisAmaya <icamayaa@eafit.edu.co>
2024-03-18 21:06:07 -0500

    static arreglado

--- countrybox/settings.py
+++ countrybox/settings.py
@@ -120,6 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATICFILES_DIRS = [
+     os.path.join(BASE_DIR, 'static'),
+     "/users/"
+]
+
 STATIC_URL = "static/"
 
 MEDIA_URL = '/media/'
